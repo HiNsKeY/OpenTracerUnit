10562b9334a1762cf1d0a39b997e066d4412af38
Sam Hinsley <sam@hinsleys.net>
2020-11-18 23:12:02 +0000

    Removed pulse logic as now completed separately. Introduced auto calibration if LEDs stay on.

--- Arduino Firmware/src/pulse.cpp
+++ Arduino Firmware/src/pulse.cpp
@@ -1,14 +1,31 @@
 #include <Arduino.h>
 #include "pulse.h"
+#include "calibration.h"
+#include "sensor.h"
+
+int maxpulses = 20;
+int pulses = 0;
 
 void pulse_main() {
-    //If output is below threshold pulse tracer
-    if ((output<(normal-threshold)) || (output>(normal+threshold)))
+    if ((pulse_enable == true) && (pulses < maxpulses))
     {
         digitalWrite(tracerled,HIGH); //Start tracer LED pulse
         delay(pulselength);
         digitalWrite(tracerled,LOW); //Stop tracer LED pulse
+        pulses++;
     }
-    else
-    {}
+    if ((pulse_enable == false) && (pulses < maxpulses))
+    {
+        digitalWrite(tracerled,LOW); //Stop tracer LED pulse
+        pulses = 0;
+    }
+    
+    if (pulses >= maxpulses)
+    {
+        digitalWrite(tracerled,LOW); //Stop tracer LED pulse
+        delay(10);
+        calibration_run();
+        pulses = 0;
+    }
+    
 }
