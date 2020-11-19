a0be1c4cf102f4a1fe6087e387acaf481ecc5593
Sam Hinsley <sam@hinsleys.net>
2020-11-19 19:56:11 +0000

    Addition of integer to count flashes as part of startup and calibration

--- Arduino Firmware/include/variables.h
+++ Arduino Firmware/include/variables.h
@@ -2,4 +2,5 @@ extern int output; //Value output by analogue phototransistor
 extern int normal; //Normal phototransistor value
 extern bool pulse_enable; //Pulse this time or not
 extern int pulses; //Pulses the tracer has completed
+extern int flashes; //Flashes counter used in startup and calibration flashes
 extern bool diag; //Diagnostic mode enable
