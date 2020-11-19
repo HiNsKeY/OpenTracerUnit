81582bccb26555554eea88668c0ee12c00111f89
Sam Hinsley <sam@hinsleys.net>
2020-11-19 19:57:16 +0000

    Changed pulse length and description to better describe "startupflash"

--- Arduino Firmware/src/config.h
+++ Arduino Firmware/src/config.h
@@ -1,6 +1,6 @@
 #define threshold 8 //Threshold for tracer activation. Lower number is more sensisitive
-#define pulselength 5 //Tracer LED pulse length in ms
-#define startupflash 100 //Startup flash length in ms
+#define pulselength 20 //Tracer LED pulse length in ms
+#define startupflash 100 //Startup & calibration flash length in ms
 #define maxpulses 20 //Maximum number of pulses before the tracer recalibrates
 
 #define diagmode false //Enable diagnostic mode to output sensor data to serial
