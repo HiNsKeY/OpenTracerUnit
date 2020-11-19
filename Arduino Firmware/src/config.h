93570ca69d04b4f94f3ece30e828023411581558
Sam Hinsley <sam@hinsleys.net>
2020-11-19 19:57:40 +0000

    Addition of new config options to change the startup and calibration pulses

--- Arduino Firmware/src/config.h
+++ Arduino Firmware/src/config.h
@@ -2,5 +2,6 @@
 #define pulselength 20 //Tracer LED pulse length in ms
 #define startupflash 100 //Startup & calibration flash length in ms
 #define maxpulses 20 //Maximum number of pulses before the tracer recalibrates
-
+#define startup_flashes 2 //Number of flashes when the unit powers on
+#define calibration_flashes 3 //Number of flashes when the unit has calibrated
 #define diagmode false //Enable diagnostic mode to output sensor data to serial
